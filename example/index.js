8f77bddc19b63e9f3edc66aacbe6636a02b7fc76
Ricky Miller <ricky.miller@gmail.com>
2016-02-15 13:41:15 -0500

    Rename options -> opts, add animation

--- example/index.js
+++ example/index.js
@@ -1,2 +1,14 @@
-var k = require('../')()
+var opts = {
+  src: 'http://i.imgur.com/YaZJZac.jpg',
+  interactive: false,
+  offsetRotation: 0.0,
+  offsetScale: 1.0,
+  offsetX: 0.0,
+  offsetY: 0.0,
+  radius: 800,
+  slices: Math.round(Math.random() * 40) + 4,
+  zoom: 0.4,
+  ease: 0.2
+}
+var k = require('../')(opts)
 console.log(k)
--- index.js
+++ index.js
@@ -1,66 +1,80 @@
-module.exports = function (options) {
+/*global Image requestAnimationFrame*/
+module.exports = function (opts) {
   var k = {}
   var half_pi = Math.PI / 2
   var two_pi = Math.PI * 2
-  // set defaults
-  options = options != null ? options : {
-    src: 'http://i.imgur.com/j40ZRls.jpg',
-    interactive: true,
-    offsetRotation: 0.0,
-    offsetScale: 1.0,
-    offsetX: 0.0,
-    offsetY: 0.0,
-    radius: 800,
-    slices: Math.round(Math.random() * 40) + 4,
-    zoom: 0.4
-  }
+  var tx = opts.offsetX
+  var ty = opts.offsetY
+  var tr = opts.offsetRotation
 
-  if (options.slices % 2) options.slices += 1 // force slices to be even
+  if (opts.slices % 2) opts.slices += 1 // force slices to be even
 
   k.domElement = document.createElement('canvas')
   k.context = k.domElement.getContext('2d')
   k.image = new Image()
-  k.image.src = options.src
+  k.image.src = opts.src
+
+  k.offsetX = opts.offsetX
+  k.offsetY = opts.offsetY
+  k.offsetRotation = opts.offsetRotation
 
   k.draw = function () {
-    k.domElement.width = k.domElement.height = options.radius * 2
+    k.domElement.width = k.domElement.height = opts.radius * 2
     k.context.fillStyle = k.context.createPattern(k.image, 'repeat')
-    var step = two_pi / options.slices
+    var step = two_pi / opts.slices
     var cx = k.image.width / 2
-    var scale = options.zoom * (options.radius / Math.min(k.image.width, k.image.height))
-    for (var i = 0; i < options.slices; i++) {
+    var scale = opts.zoom * (opts.radius / Math.min(k.image.width, k.image.height))
+    for (var i = 0; i < opts.slices; i++) {
       k.context.save()
-      k.context.translate(options.radius, options.radius)
+      k.context.translate(opts.radius, opts.radius)
       k.context.rotate(i * step)
       k.context.beginPath()
       k.context.moveTo(-0.5, -0.5)
-      k.context.arc(0, 0, options.radius, step * -0.51, step * 0.51)
+      k.context.arc(0, 0, opts.radius, step * -0.51, step * 0.51)
       k.context.lineTo(0.5, 0.5)
       k.context.closePath()
-      console.log(half_pi)
       k.context.rotate(half_pi)
       k.context.scale(scale, scale)
       k.context.scale([-1, 1][i % 2], 1)
-      k.context.translate(options.offsetX - cx, options.offsetY)
-      k.context.rotate(options.offsetRotation)
-      k.context.scale(options.offsetScale, options.offsetScale)
+      k.context.translate(k.offsetX - cx, k.offsetY)
+      k.context.rotate(k.offsetRotation)
+      k.context.scale(k.offsetScale, k.offsetScale)
       k.context.fill()
       k.context.restore()
     }
   }
 
-  k.image.onload = function() {
+  k.image.onload = function () {
     k.draw()
   }
 
   k.domElement.style.position = 'absolute'
-  k.domElement.style.marginLeft = -options.radius + 'px'
-  k.domElement.style.marginTop = -options.radius + 'px'
+  k.domElement.style.marginLeft = -opts.radius + 'px'
+  k.domElement.style.marginTop = -opts.radius + 'px'
   k.domElement.style.left = '50%'
   k.domElement.style.top = '50%'
   k.domElement.setAttribute('class', 'kaleidoscope')
 
   document.body.appendChild(k.domElement)
+
+  function render () {
+    var delta, theta
+    if (!opts.interactive) {
+      var time = new Date().getTime() * 0.0002
+      tx = Math.sin(time) * 1920 + 2560
+      ty = Math.cos(time * 0.9) * 1920 + 2560
+    }
+
+    delta = tr - k.offsetRotation
+    theta = Math.atan2(Math.sin(delta), Math.cos(delta))
+    k.offsetX += (tx - k.offsetX) * opts.ease
+    k.offsetY += (ty - k.offsetY) * opts.ease
+    k.offsetRotation += (theta - k.offsetRotation) * opts.ease
+    k.draw()
+    requestAnimationFrame(render)
+  }
+
+  render()
 
   return k
 }
