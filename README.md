dcc3e513dbbe3d2c5dd0992c34f3c6f3df676658
Ricky Miller <ricky.miller@gmail.com>
2016-02-23 22:04:07 -0500

    Add tests

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 `<canvas>` kaleidoscope node module for use with browserify, webpack, etc.
 
 #### TODO
-Add tests  
+~~Add tests~~  
+Add _more_ tests  
 Add more examples  
 ~~Fix webcam example in Firefox~~  
 ~~Update src to be image _or_ video~~  
-Cleanup  
+~~Cleanup~~
---
+++ test/test.js
@@ -0,0 +1,44 @@
+/*global Image */
+const test = require('tape')
+const Kaleidos = require('../')
+
+const image = new Image()
+image.src = 'http://i.imgur.com/YaZJZac.jpg'
+const conf = {
+  className: 'kaleidos',
+  src: image,
+  offsetRotation: 0,
+  offsetScale: 1,
+  offsetX: 0,
+  offsetY: 0,
+  radius: 800,
+  slices: Math.round(Math.random() * 20) + 4,
+  zoom: 0.4,
+  ease: 0.1
+}
+
+const kaleidos = new Kaleidos(conf)
+console.log(kaleidos)
+
+test('offsetRotation should update', function (t) {
+  t.plan(1)
+  var value = 0.4
+  kaleidos.offsetRotation = value
+  t.equal(kaleidos.offsetRotation, value)
+})
+
+test('offsetX should update', function (t) {
+  t.plan(1)
+  var initial = kaleidos.offsetX
+  var value = 0.4
+  kaleidos.offsetX += value
+  t.equal(kaleidos.offsetX, initial + value)
+})
+
+test('offsetY should update', function (t) {
+  t.plan(1)
+  var initial = kaleidos.offsetY
+  var value = 0.8
+  kaleidos.offsetY += value
+  t.equal(kaleidos.offsetY, initial + value)
+})
