89ffa67cdd22d70557352db7f317ad9d1db9a872
Ricky Miller <ricky.miller@gmail.com>
2016-03-17 01:46:15 -0400

    Update example

--- examples/apod/index.js
+++ examples/apod/index.js
@@ -1,7 +1,52 @@
 /*global Image XMLHttpRequest*/
-const Kaleidos = require('../../')
-const amount = 40
-const apodUrl = 'https://api.nasa.gov/planetary/apod?api_key=ZeHs1xlBAtEBIAhcvT2aN6puHvknYEh9rcquGhLE&date='
+var Kaleidos = require('../../')
+var amount = 40
+var api_key = 'ZeHs1xlBAtEBIAhcvT2aN6puHvknYEh9rcquGhLE'
+var apod_url = 'https://api.nasa.gov/planetary/apod?api_key=' + api_key + '&date='
+var html
+var body
+var overlay
+var overlay_content
+var overlay_bg
+var content
+
+function qs (selector) {
+  return document.querySelector(selector)
+}
+
+function overlayTemplate (data) {
+  return [
+    '<h2>',
+    data.title,
+    '</h2><h3>Date: ',
+    data.date,
+    '</h3><img style="max-width: 100%;" src="',
+    data.hdurl || data.url,
+    '?api_key="' + api_key + '" /><p>',
+    data.explanation,
+    '</div>'
+  ].join('')
+}
+
+function getPageHeight () {
+  return Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight, html.offsetHeight
+  ) + 'px'
+}
+
+function toggleOverlay (event) {
+  event.preventDefault()
+  var toggleClass = 'overlay-enabled'
+  body.classList.toggle(toggleClass)
+  if (body.classList.contains(toggleClass)) {
+    overlay_content.innerHTML = this.dataset.overlay
+    overlay.style.height = getPageHeight()
+    body.scrollTop = 0
+  }
+}
 
 function randomDate () {
   var start = new Date(1996, 6, 16)
@@ -17,13 +62,15 @@ function getPhoto (url) {
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText)
+
       var image = new Image()
       image.src = data.url
+
       var kaleidos = new Kaleidos({
         className: data.url,
         radius: 400,
         src: image,
-        slices: Math.round(Math.random() * 80) + 4,
+        slices: Math.round(Math.random() * 20) + 4,
         style: false
       })
 
@@ -31,36 +78,12 @@ function getPhoto (url) {
       a.setAttribute('title', data.title)
       a.setAttribute('target', '_blank')
       a.setAttribute('class', 'mandala')
-
-      a.addEventListener('click', function (event) {
-        event.preventDefault()
-        var w = window.open('about:blank', data.url)
-        var t = [
-          '<body style="background:#2B2B2B;">',
-          '<div style="padding:20;font-family:sans-serif;color:#F7F7F7;">',
-          '<h2>',
-          data.title,
-          '</h2>',
-          '<h3>',
-          'Date: ',
-          data.date,
-          '</h3>',
-          '<img style="max-width: 100%;" src="',
-          data.hdurl || data.url,
-          '?api_key=ZeHs1xlBAtEBIAhcvT2aN6puHvknYEh9rcquGhLE" />',
-          '<p>',
-          data.explanation,
-          '</p>',
-          '</div>',
-          '</body>'
-        ]
-        w.document.write(t.join(''))
-      })
-
+      a.setAttribute('data-overlay', overlayTemplate(data))
+      a.addEventListener('click', toggleOverlay)
       a.appendChild(kaleidos.domElement)
 
       image.addEventListener('load', function () {
-        document.body.appendChild(a)
+        content.appendChild(a)
         kaleidos.init()
       })
     }
@@ -73,9 +96,18 @@ function getPhoto (url) {
 }
 
 function init () {
-  document.body.style.overflow = 'visible'
+  body = document.body
+  html = document.documentElement
+  var close = qs('.close')
+  overlay = qs('.overlay')
+  overlay_content = qs('.overlay .wrap div')
+  content = qs('.content')
+  overlay_bg = qs('.bg')
+  close.addEventListener('click', toggleOverlay)
+  overlay_bg.addEventListener('click', toggleOverlay)
+  body.style.overflow = 'visible'
   for (var i = 0; i < amount; i++) {
-    getPhoto(apodUrl + randomDate())
+    getPhoto(apod_url + randomDate())
   }
 }
 
